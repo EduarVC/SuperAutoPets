94907ccb661639e5ff859f22710dfae86c3e4440
EduarVC <eduardodavid-vasquezcabrera@cunoc.edu.gt>
2022-03-14 19:55:20 -0600

    Se llenaron datos Ballena

--- src/main/java/com/mycompany/Acuatico/Ballena.java
+++ src/main/java/com/mycompany/Acuatico/Ballena.java
@@ -3,4 +3,10 @@ import com.mycompany.Mascotas.Acuatico;
 
 public class Ballena extends Acuatico{
     
+    public Ballena(){
+        nombreMascota = "Ballena";
+        habilidad = "Succion";
+        puntosAtaque = 3;
+        puntosVida = 8;
+    }
 }
--- src/main/java/com/mycompany/Mascotas/Mascota.java
+++ src/main/java/com/mycompany/Mascotas/Mascota.java
@@ -1,37 +1,40 @@
-
 package com.mycompany.Mascotas;
 
-
 public class Mascota {
-    
+
     //Atributos generales
-    private String nombreMascota;
-    private String abilidad;
-    private String tipoMascota;
-    private int nivel;
-    private int puntosVida;
-    private int puntosDaño;
-    private int dañoRecivido;
-    private boolean efecto;
-    private int experiencia;
-   
-    public void Atacar(){
-        
+    protected String nombreMascota;
+    protected String habilidad;
+    protected String tipoMascota;
+    protected int nivel;
+    protected int puntosVida;
+    protected int puntosAtaque;
+    protected int dañoRecivido;
+    protected boolean efecto;
+    protected int experiencia;
+
+    public void Atacar() {
+
     }
-    public void Fusionarse(){
-        
+
+    public void Fusionarse() {
+
     }
-    public void Alimentarce(){
-        
+
+    public void Alimentarce() {
+
     }
-    public void RecivirDaño(){
-        
+
+    public void RecivirDaño() {
+
     }
-    public void CambiarNivel(){
-        
+
+    public void CambiarNivel() {
+
     }
-    public void CambiarEfecto(){
-        
+
+    public void CambiarEfecto() {
+
     }
-    
+
 }
